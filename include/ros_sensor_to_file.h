2a3eeda3adb8dc3d7a4a56d33cb9576a843a12b4
ATTY Lionel <lionel.atty@ign.fr>
2016-07-05 15:36:43 +0200

    Gros refactoring pour prendre en compte une extériorisation dans la lib 'subscriber_to_topic'.

--- include/ros_sensor_to_file.h
+++ include/ros_sensor_to_file.h
@@ -4,26 +4,17 @@
 #include <string>
 #include <fstream>
 
-//#include "ros_boost_serialize.h"
 #include "ros_boost_serialize_sensors.h"
-#include "ros_macros.h"
 
-#include <ros/node_handle.h>
-
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/binary_oarchive.hpp>
-#include <boost/archive/binary_iarchive.hpp>
-//
-#include <boost/archive/xml_oarchive.hpp>
-
+#include <subscriber_to_file/subscriber_to_file.h>
 
 using namespace ros_boost_serialize::sensors;
 
 /**
  * @brief The SensorsToFile class
  */
-class SensorsToFile {
+class SensorsToFile : public SubToFile< ::sensor_msgs::Imu, _sensor_msgs_serialize_type >
+{
 public:
 
     /**
@@ -34,6 +25,13 @@ public:
     SensorsToFile (ros::NodeHandle _priv_nh = ros::NodeHandle("~"), const uint32_t& _size_buffer=50 );
 
     /**
+     * @brief build_filename
+     * @return
+     */
+    std::string build_filename() const override;
+
+protected:
+    /**
      * @brief get_extension
      * @return
      * Si XML export => extension du fichier = "xml"
@@ -42,33 +40,8 @@ public:
      */
     inline std::string get_extension() const { return xml_ ? ".xml" : binary_ ? ".dat" : ".txt"; }
 
-    std::string build_filename() const;
-
-    template<typename T, class Archive> static void dump(const std::vector<T> & _vector, Archive & _ar);
-
-    bool dump(const std::string & _filename);
-    //
-    inline void dump(boost::archive::xml_oarchive & _ar) const { _ar & BOOST_SERIALIZATION_NVP(v_sensor_msgs_); }
-    inline void dump(boost::archive::binary_oarchive & _ar) const { dump<_sensor_msgs_serialize_type, boost::archive::binary_oarchive>(v_sensor_msgs_, _ar); }
-    inline void dump(boost::archive::text_oarchive & _ar) const { dump<_sensor_msgs_serialize_type, boost::archive::text_oarchive>(v_sensor_msgs_, _ar); }
-
-    void sensors_cb(const sensor_msgs::ImuConstPtr& _msg);
-
-    std::string sensors_topic_;
-
-    ros::Subscriber sub_;
-
-protected:
-    ros::NodeHandle nh_;
-
 private:
     std::string prefix_;
-
-    bool binary_;
-    bool show_msg_;
-    bool xml_;
-
-    std::vector<_sensor_msgs_serialize_type> v_sensor_msgs_;
 };
 
-#endif // ROS_SENSOR_TO_FILE_H
+#endif
--- src/ros_sensor_to_file.cpp
+++ src/ros_sensor_to_file.cpp
@@ -2,103 +2,25 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
-SensorsToFile::SensorsToFile (ros::NodeHandle _priv_nh,const uint32_t& _size_buffer)
+SensorsToFile::SensorsToFile (ros::NodeHandle _priv_nh, const uint32_t& _size_buffer) :
+    SubToFile(_size_buffer)
 {
-    v_sensor_msgs_.reserve(_size_buffer);
-
     // On recupere les parametres ROS
     GET_ROS_PARAM(prefix, prefix_, "SENSORS_", _priv_nh, nh_);
-    GET_ROS_PARAM(binary, binary_, true, _priv_nh, nh_);
-    GET_ROS_PARAM(xml, xml_, false, _priv_nh, nh_);
-    GET_ROS_PARAM(sensors_topic, sensors_topic_, "/android/imu", _priv_nh, nh_);
-    GET_ROS_PARAM(show_msg, show_msg_, false, _priv_nh, nh_);
 
-    // On lance le subscriber
-    sub_ = nh_.subscribe(sensors_topic_, 1, &SensorsToFile::sensors_cb, this);
-
-    ROS_INFO ("Listening for incoming data on topic %s",
-              nh_.resolveName (sensors_topic_).c_str ());
+    // Lance le subscriber ros sur le topic
+    run();
 }
 
 std::string SensorsToFile::build_filename() const
 {
     // On recupere le timestamp du message
-    const ros::Time& stamp_begin = v_sensor_msgs_.front().stamp();
-    const ros::Time& stamp_end = v_sensor_msgs_.back().stamp();
+    const ros::Time& stamp_begin = v_msgs_.front().stamp();
+    const ros::Time& stamp_end = v_msgs_.back().stamp();
     // On construit le nom du fichier image par rapport a ce timestamp
     std::stringstream ss_sensors_filename;
     ss_sensors_filename << prefix_ <<                               \
                            stamp_begin << "_to_" << stamp_end <<    \
                            get_extension();
     return ss_sensors_filename.str();
-}
-
-template<typename T, class Archive>
-void SensorsToFile::dump(const std::vector<T> & _vector, Archive & _ar)
-{
-    // dump vector to archive
-    for (typename std::vector<T>::const_iterator it = _vector.begin() ; it != _vector.end(); ++it)
-        _ar << *it;
-}
-
-bool SensorsToFile::dump(const std::string & _filename)
-{
-    bool retour = true;
-
-    std::ofstream ofs(_filename, std::ios::out | std::ofstream::binary);
-    if(ofs.is_open()) {
-        // save data to archive
-        try {
-            if(xml_) {
-                boost::archive::xml_oarchive oa(ofs);
-                dump(oa);
-            }
-            else {
-                if(binary_) {
-                    boost::archive::binary_oarchive oa(ofs);
-                    // write vector to archive
-                    dump(oa);
-                    // archive and stream closed when destructors are called
-                }
-                else {
-                    boost::archive::text_oarchive oa(ofs);
-                    // write vector to archive
-                    dump(oa);
-                    // archive and stream closed when destructors are called
-                }
-            }
-        }
-        catch(std::exception &exc){
-            ROS_ERROR_STREAM("Exception lors de la serialization ! -> " << exc.what());
-            retour = false;
-        }
-
-        ROS_INFO_STREAM("Write sensors data into: " << _filename);
-    }
-    else {
-        ROS_ERROR_STREAM("Probleme d'ouverture du stream: " << _filename);
-        retour = false;
-    }
-
-    return retour;
-}
-
-void SensorsToFile::sensors_cb(const sensor_msgs::ImuConstPtr& _msg)
-{
-    if (show_msg_)
-        ROS_INFO_STREAM("Orientation\n" << _msg->orientation);
-
-    // Sauvegarde en memoire du message (les informations qui nous interessent du message)
-    _sensor_msgs_serialize_type sensor_msg(_msg);
-    v_sensor_msgs_.push_back(sensor_msg);
-    //        ROS_INFO_STREAM("sensor_msg : " << sensor_msg);
-
-    // Est ce qu'on a atteint la capacite max. du vecteur ?
-    if (v_sensor_msgs_.size() == v_sensor_msgs_.capacity())
-    {
-        // Si oui, on enregistre (sur fichier) le vecteur
-        dump(build_filename());
-        // et on le vide
-        v_sensor_msgs_.clear();
-    }
 }
